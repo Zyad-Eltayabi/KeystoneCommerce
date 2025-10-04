32c8c23df6fa26af61d94c770286dbfe017e9dec
Zyad-Mahmoud <ZyadEltayibi@gmail.com>
2025-10-04 23:08:47 +0300

    add validation for banner type in create banner view
    - Introduce ValidBannerTypeAttribute for validating banner type
    - Apply attribute to BannerType property in CreateBannerViewModel
    - Add BannerTypeNames property for dropdown selection in view
    - Update Create.cshtml to include dropdown for banner type
    - Ensure validation messages are displayed for user feedback

---
+++ KeystoneCommerce.WebUI/Attributes/ValidBannerTypeAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace KeystoneCommerce.WebUI.Attributes
+{
+    public class ValidBannerTypeAttribute : ValidationAttribute
+    {
+        private readonly List<int> _bannerTypesValues;
+        public ValidBannerTypeAttribute()
+        {
+            _bannerTypesValues = new List<int> { 1, 2, 3 };
+        }
+        protected override ValidationResult? IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is not int intValue)
+            {
+                return new ValidationResult("Invalid Banner Type");
+            }
+
+            if (!_bannerTypesValues.Contains(intValue))
+            {
+                return new ValidationResult("The value must be between 1 and 3");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
