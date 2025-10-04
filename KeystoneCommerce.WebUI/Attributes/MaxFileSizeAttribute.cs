74a3f71bb55e7beaa5c9d0b8f4a27cd0be110c18
Zyad-Mahmoud <ZyadEltayibi@gmail.com>
2025-10-04 23:04:23 +0300

    add file size validation attributes and constants
    - Introduced `MaxFileSizeAttribute` for validating uploaded file sizes
    - Added `ConvertBytesToReadableSize` method for user-friendly size display
    - Created `FileSizes` class with a constant for maximum image size (1 MB)

---
+++ KeystoneCommerce.WebUI/Attributes/MaxFileSizeAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KeystoneCommerce.WebUI.Attributes
+{
+    public class MaxFileSizeAttribute : ValidationAttribute
+    {
+        private readonly int _maxFileSizeInBytes;
+        public MaxFileSizeAttribute(int maxFileSizeInBytes)
+        {
+            _maxFileSizeInBytes = maxFileSizeInBytes;
+        }
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is IFormFile file)
+            {
+                if (file.Length > _maxFileSizeInBytes)
+                {
+                    return new ValidationResult(ErrorMessage ?? $"The file size exceeds the maximum allowed size of {ConvertBytesToReadableSize(_maxFileSizeInBytes)}.");
+                }
+            }
+            return ValidationResult.Success;
+        }
+
+        private string ConvertBytesToReadableSize(long byteCount)
+        {
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
+            if (byteCount == 0)
+                return "0" + suf[0];
+            long bytes = Math.Abs(byteCount);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return (Math.Sign(byteCount) * num).ToString() + suf[place];
+        }
+    }
+}
---
+++ KeystoneCommerce.WebUI/Constants/FileSizes.cs
@@ -0,0 +1,7 @@
+﻿namespace KeystoneCommerce.WebUI.Constants
+{
+    public class FileSizes
+    {
+        public const int MaxImageSizeInByte = 1 * 1024 * 1024;
+    }
+}
