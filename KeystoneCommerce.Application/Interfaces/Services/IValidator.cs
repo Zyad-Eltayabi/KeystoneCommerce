8e1892a1fb519a7b09282f675e1436e8687601d9
Zyad-Mahmoud <ZyadEltayibi@gmail.com>
2025-10-06 06:51:43 +0300

    add validation result class and validator interface
    - Introduced ApplicationValidationResult class for validation results
    - Added IApplicationValidator<T> interface for instance validation
    - Created DTOs folder structure in project for organization

---
+++ KeystoneCommerce.Application/Interfaces/Services/IValidator.cs
@@ -0,0 +1,10 @@
+﻿using KeystoneCommerce.Application.Common.Validation;
+
+namespace KeystoneCommerce.Application.Interfaces.Services
+{
+    public interface IApplicationValidator<in T>
+    {
+        Task<ApplicationValidationResult> ValidateAsync(T instance);
+        ApplicationValidationResult Validate(T instance);
+    }
+}
