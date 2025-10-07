26cc71a89471db28adb9b48ab1dbd6e07ba81bfd
Zyad-Mahmoud <ZyadEltayibi@gmail.com>
2025-10-07 03:11:48 +0300

    add file paths class and update connection string
    - Introduced `FilePaths` class in `KeystoneCommerce.WebUI.Constants`
    - Added constants for image root and banner path
    - Updated `DefaultConnection` in `appsettings.json` to use local server

---
+++ KeystoneCommerce.WebUI/Constants/FilePaths.cs
@@ -0,0 +1,8 @@
+﻿namespace KeystoneCommerce.WebUI.Constants
+{
+    public class FilePaths
+    {
+        public const string imageRoot = "wwwroot/assets/img";
+        public const string bannerPath = imageRoot + "/banners";
+    }
+}
