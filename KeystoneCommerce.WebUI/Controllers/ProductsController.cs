a7cb8300ef048773c2aa89ea2723547731199110
Zyad-Mahmoud <ZyadEltayibi@gmail.com>
2025-10-14 15:23:40 +0300

    add products management functionality
    - Introduced a new ProductsController with an Index action
    - Updated project file to include a non-breaking space character
    - Changed scaffolder settings in user project file for views and controllers
    - Modified Index.cshtml to set layout and display product creation button
    - Updated navigation in _AdminLayout to link to products index

---
+++ KeystoneCommerce.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KeystoneCommerce.WebUI.Controllers
+{
+    [Route("Admin/[controller]")]
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
