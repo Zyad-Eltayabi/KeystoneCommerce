7a9122099dec981dc24c23448c1fd8f2f6267cb0
Zyad-Mahmoud <ZyadEltayibi@gmail.com>
2025-12-02 21:56:13 +0200

    enhance checkout process with validation and shipping details

--- KeystoneCommerce.WebUI/ViewModels/Checkout/CreateCheckoutViewModel.cs
+++ KeystoneCommerce.WebUI/ViewModels/Checkout/CreateCheckoutViewModel.cs
@@ -8,4 +8,7 @@ public class CreateCheckoutViewModel
     public List<ProductCartViewModel>? Products { get; set; } = new();
     public ApplyCouponViewModel ApplyCoupon { get; set; } = new();
     public List<ShippingMethodViewModel> ShippingMethods { get; set; } = new();
+    public ShippingDetailsViewModel ShippingDetails { get; set; } = new();
+    public string ShippingMethod { get; set; } = string.Empty;
+    public string CouponCode { get; set; } = string.Empty;
 }
