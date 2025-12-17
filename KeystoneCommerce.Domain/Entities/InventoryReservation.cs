0eeffecd1c4799a236778136d5b690680dc3d71c
Zyad-Mahmoud <ZyadEltayibi@gmail.com>
2025-12-17 17:28:48 +0200

    make ExpiresAt nullable in InventoryReservation entity

--- KeystoneCommerce.Domain/Entities/InventoryReservation.cs
+++ KeystoneCommerce.Domain/Entities/InventoryReservation.cs
@@ -6,7 +6,7 @@ public class InventoryReservation
 {
     public int Id { get; set; }
     public int OrderId { get; set; }
-    public DateTime ExpiresAt { get; set; }
+    public DateTime? ExpiresAt { get; set; }
     public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
     public ReservationStatus Status { get; set; }
     public Order Order { get; set; } = null!;
